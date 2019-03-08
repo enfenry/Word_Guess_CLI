7dc4e84b2f4d403c0a17bc4781ce7031c08718dd
enfenry <enfenry@vt.edu>
2019-03-08 14:25:41 -0500

    removed unnecessary comments

--- Letter.js
+++ Letter.js
@@ -1,13 +1,3 @@
-// * **Letter.js**: Contains a constructor, Letter. This constructor should be able to either display an underlying character or a blank placeholder (such as an underscore), depending on whether or not the user has guessed the letter. That means the constructor should define:
-
-//   * A string value to store the underlying character for the letter
-
-//   * A boolean value that stores whether that letter has been guessed yet
-
-//   * A function that returns the underlying character if the letter has been guessed, or a placeholder (like an underscore) if the letter has not been guessed
-
-//   * A function that takes a character as an argument and checks it against the underlying character, updating the stored boolean value to true if it was guessed correctly
-
 class Letter {
     constructor (character,guessed) {
         this.character = character;
@@ -42,28 +32,3 @@ class Letter {
 }
 
 module.exports = Letter;
-
-// let A = new Letter('a',false);
-// let B = new Letter('B',false);
-// let C = new Letter('C',false);
-
-// A.check('a');
-// B.check('a');
-// C.check('a');
-
-// // console.log(A,B,C);
-// console.log(A.toString(),B.toString(),C.toString());
-
-// A.check('B');
-// B.check('B');
-// C.check('B');
-
-// // console.log(A,B,C);
-// console.log(A.toString(),B.toString(),C.toString());
-
-// A.check('c');
-// B.check('c');
-// C.check('c');
-
-// // console.log(A,B,C);
-// console.log(A.toString(),B.toString(),C.toString());
--- index.js
+++ index.js
@@ -1,13 +1,3 @@
-// * **index.js**: The file containing the logic for the course of the game, which depends on `Word.js` and:
-
-//   * Randomly selects a word and uses the `Word` constructor to store it
-
-//   * Prompts the user for each guess and keeps track of the user's remaining guesses
-
-// 3. `Letter.js` *should not* `require` any other files.
-
-// 4. `Word.js` *should only* require `Letter.js`
-
 let Letter = require('./Letter');
 let Word = require('./Word');
 let inquirer = require("inquirer");
